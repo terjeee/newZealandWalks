8a8bf245b14b2cfc76e5be00a4b28b1f323b6f1d
Terje O <terje.osevelle@sharebox.no>
2024-04-26 19:41:17 +0200

    Add project files.

---
+++ newZealandWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace newZealandWalks.API.Controllers
+{
+    [Route("api/[controller]")] // https://localhost:PORT/api/students
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet] // GET: https://localhost:PORT/api/students
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Jon", "Jon", "Jon", "Jon", "Jon" };
+
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ newZealandWalks.API/Data/newZealandWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using newZealandWalks.API.Models.Domain;
+
+namespace newZealandWalks.API.Data
+{
+    public class newZealandWalksDbContext: DbContext
+    {
+        public newZealandWalksContext(DbContextOptions dbContextOptions): base(DbContextOptions)
+        {
+
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
