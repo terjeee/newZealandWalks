6d383f17fcf7730a3c8dd1cb0ee1cf803b459b9e
Terje O <terje.osevelle@sharebox.no>
2024-05-07 20:05:04 +0200

    add: PUT region (neither fields mandatory)

---
+++ newZealandWalks.API/Models/DTO/UpdateRegionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace newZealandWalks.API.Models.DTO
+{
+    public class UpdateRegionDTO
+    {
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
